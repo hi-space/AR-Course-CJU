2586b36c8d95e2f1a28d03ad82135fe2c9f18a80
hi-space <hispace.j@gmail.com>
2021-08-02 23:54:22 +0900

    ar raycast scene

---
+++ ARCourse/Assets/Scripts/ARFixedObjectOnPlane.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARFixedObjectOnPlane : MonoBehaviour
+{
+    private ARRaycastManager arRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private GameObject spawnedObject;
+
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+        {
+            return;
+        }
+
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began &&
+            arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneEstimated))
+        {
+            var hitPose = hits[0].pose;
+
+            if (!spawnedObject)
+            {
+                spawnedObject = Instantiate(arRaycastManager.raycastPrefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+                spawnedObject.transform.rotation = hitPose.rotation;
+            }
+        }
+    }
+}
---
+++ ARCourse/Assets/Scripts/ARObjectOnPlane.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARObjectOnPlane : MonoBehaviour
+{
+    private ARRaycastManager arRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+        {
+            return;
+        }        
+
+        if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            Instantiate(arRaycastManager.raycastPrefab, hitPose.position, hitPose.rotation);
+        }
+    }
+}
