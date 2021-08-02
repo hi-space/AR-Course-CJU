cf28c14cac092ac4441d31f1b57aadd759b48641
hi-space <hispace.j@gmail.com>
2021-08-02 22:51:24 +0900

    plane tracking & shoot ar bullet

---
+++ ARCourse/Assets/Scripts/ARShooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARShooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletObject;
+
+    private Transform cameraTransform;
+
+    private void Awake()
+    {
+        cameraTransform = GetComponentInChildren<Camera>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            GameObject obj = Instantiate(bulletObject);
+            obj.transform.position = cameraTransform.position;
+            obj.GetComponent<Rigidbody>().AddForce(cameraTransform.forward * 100f);
+        }
+    }
+}
