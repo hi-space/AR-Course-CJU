b1386bd46290e504c1040c1760761bdcf29b4d6f
hi-space <hispace.j@gmail.com>
2021-08-06 00:20:03 +0900

    occlusion & scale, rotation

--- ARCourse/Assets/Scripts/ARObjectController.cs
+++ ARCourse/Assets/Scripts/ARObjectController.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-public class ARObjectMovement : MonoBehaviour
+public class ARObjectController : MonoBehaviour
 {
     private ARRaycastManager arRaycastManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     private GameObject arObject;
 
+    private float scale = 1.0f;
+    private float angle = 0.0f;
+
     private void Awake()
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    public void UpdateScale(float sliderValue)
+    {
+        scale = sliderValue;
+
+        if (arObject)
+        {
+            arObject.transform.localScale = Vector3.one * scale;
+        }        
+    }
+
+    public void UpdateRotation(float sliderValue)
+    {
+        angle = sliderValue;
+
+        if (arObject)
+        {
+            // arObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+            arObject.transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
+        }
     }
 
     void Update()
@@ -21,7 +45,7 @@ public class ARObjectMovement : MonoBehaviour
         if (Input.touchCount == 0)
         {
             return;
-        }
+        }        
 
         if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
         {
@@ -30,12 +54,15 @@ public class ARObjectMovement : MonoBehaviour
             if (!arObject)
             {
                 arObject = Instantiate(arRaycastManager.raycastPrefab, hitPose.position, hitPose.rotation);
+
+                arObject.transform.localScale = Vector3.one * scale;
+                arObject.transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0));
             }
             else
             {
                 arObject.transform.position = hitPose.position;
                 arObject.transform.rotation = hitPose.rotation;
-            }
+            }            
         }
     }
 }
